2298ef543368db01fbb8ed14e0bb41621b44d936
Kevin dsa <kevin0108dsa@gmail.com>
2022-08-26 09:43:43 -0400

    Update mailer.js

--- data/mailer.js
+++ data/mailer.js
@@ -31,3 +31,29 @@ const ReportService = {
 };
 
 export default ReportService;
+
+
+// try ZOHO MAIL
+
+/*
+I used Zoho mail and it works fine. Make sure to use the same Zoho email address in the from field of mailOptions as it will throw an error otherwise.
+
+var transport = nodemailer.createTransport({
+    host: 'smtp.zoho.eu',
+    port: 465,
+    secure: true, //ssl
+    auth: {
+        user:process.env.EMAIL,
+        pass:process.env.EMAIL_PASSWORD
+    }
+});
+
+var mailOptions = {
+    from: process.env.EMAIL,
+    to: req.body.email,
+    subject: "Subject",
+    html: ""
+};
+Generally Gmail is not the preferred service to use with Nodemailer since it is targeted at real users and not for automatic/programmed use. See Nodemailer with Gmail for more info. Also here is a list of alternative email services supported by Nodemailer WELL-KNOWN SERVICES.
+
+*/
